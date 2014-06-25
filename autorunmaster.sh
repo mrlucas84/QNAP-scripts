5ad80059e57eebda0659411070737e62cda82242
danirod <8984@bancamarch.es>
2014-06-25 18:20:43 +0200

    cambiado output via ts a echo date

--- autorunmaster.sh
+++ autorunmaster.sh
@@ -3,12 +3,12 @@
 # con /bin/sh no funciona el redireccionamiento a funcion/process substitution
 # this is called by autorun.sh
 # /share/HDA_DATA/.qpkg/autorun/autorunmaster.sh
+#alias ts='/opt/bin/ts'
 log=/share/HDA_DATA/.qpkg/autorun/autorunmaster.log
 stdlog=/share/HDA_DATA/.qpkg/autorun/autorunmaster_std.log
-echo "*** Starting autorunmaster.sh" | ts "%F %H:%M:%.S" >> $log
+echo "$(date '+%F %T') *** Starting autorunmaster.sh" >> $log
 #namedpipe=/share/HDA_DATA/.qpkg/autorun/autorunmaster.sh.pipe
 apache_conf=/etc/config/apache/apache.conf
-#alias ts='/opt/bin/ts'
 #if [ -p $namedpipe ]; then
 #	rm -f "$namedpipe"
 #fi
@@ -30,19 +30,19 @@ exec > $stdlog 2>&1
 # Bug fix for following: put IPKG first, per http://forum.qnap.com/viewtopic.php?f=124&t=15663
 # was [ $? -ne 0 ] && /bin/echo "export PATH=$PATH":/opt/bin:/opt/sbin >> /etc/profile
 #[ $? -ne 0 ] && /bin/echo "export PATH=/opt/bin:/opt/sbin:\$PATH" >> /etc/profile
-echo "PATH=$PATH" | ts "%F %H:%M:%.S" >> $log
+echo "$(date '+%F %T') PATH=$PATH" >> $log
 
 #Dani 12/11/2011 modificado segun http://forum.qnap.com/viewtopic.php?f=85&t=18977
 #FIRST start Optware and delete the /etc/rcS.d/QS100...sh
-echo "Starting Optware" | ts "%F %H:%M:%.S" >> $log
-/etc/init.d/Optware.sh start | ts "%F %H:%M:%.S" >> $log
+echo "$(date '+%F %T') Starting Optware" >> $log
+/etc/init.d/Optware.sh start >> $log
 rm -f /etc/rcS.d/QS100Optware
-echo "Optware started" | ts "%F %H:%M:%.S" >> $log
-echo "Setting up custom scripts" | ts "%F %H:%M:%.S" >> $log
+echo "$(date '+%F %T') Optware started" >> $log
+echo "$(date '+%F %T') Setting up custom scripts" >> $log
 # Fin Dani 12/11/2011
 
 #sobreescribir config SSH con la propia 
-echo "Delete /etc/ssh/sshd_config and recreate as symlink to /share/HDA_DATA/ssh/sshd_config" | ts "%F %H:%M:%.S" >> $log
+echo "$(date '+%F %T') Delete /etc/ssh/sshd_config and recreate as symlink to /share/HDA_DATA/ssh/sshd_config" >> $log
 rm -f /etc/ssh/sshd_config
 ln -s /share/HDA_DATA/ssh/sshd_config /etc/ssh/sshd_config
 
@@ -50,40 +50,40 @@ ln -s /share/HDA_DATA/ssh/sshd_config /etc/ssh/sshd_config
 /bin/grep "apache-custom" $apache_conf >/dev/null
 if [ $? = 0 ]
 	then
-		echo "$apache_conf does NOT contain customizations. Including now." | ts "%F %H:%M:%.S" >> $log
-		echo "Include /share/HDA_DATA/apache/apache-custom.conf" >> $apache_conf
-		echo "Restarting apache" | ts "%F %H:%M:%.S" >> $log
-		/etc/init.d/Qthttpd.sh restart | ts "%F %H:%M:%.S" >> $log
+		echo "$(date '+%F %T') $apache_conf does NOT contain customizations. Including now." >> $log
+		echo "$(date '+%F %T') Include /share/HDA_DATA/apache/apache-custom.conf" >> $apache_conf
+		echo "$(date '+%F %T') Restarting apache" >> $log
+		/etc/init.d/Qthttpd.sh restart >> $log
 	else
-		echo "$apache_conf already contains customizations. Nothing to be done." | ts "%F %H:%M:%.S" >> $log
+		echo "$(date '+%F %T') $apache_conf already contains customizations. Nothing to be done." >> $log
 fi
-#echo "Copying apache SSL custom conf" | ts "%F %H:%M:%.S" >> $log
+#echo "Copying apache SSL custom conf" >> $log
 #cp -f /share/HDA_DATA/apache/extra/apache-ssl.conf /etc/config/apache/extra/apache-ssl.conf
 
-#echo "Restarting apache" | ts "%F %H:%M:%.S" >> $log
-#/etc/init.d/Qthttpd.sh restart | ts "%F %H:%M:%.S" >> $log
+#echo "Restarting apache" >> $log
+#/etc/init.d/Qthttpd.sh restart >> $log
 
 # crea enlace a script de transmission para que se ejecute cuando toca.
 #echo "Creating symlink /etc/rcS.d/QS901transmission -> /share/HDA_DATA/Transmission/transmission.sh"
 #/bin/ln -sf /share/HDA_DATA/Transmission/transmission.sh /etc/rcS.d/QS901transmission
 
 #Dani 12/11/2011
-echo "Calling Transmission script /share/HDA_DATA/Transmission/transmission.sh"
+echo "$(date '+%F %T') Calling Transmission script /share/HDA_DATA/Transmission/transmission.sh"
 /share/HDA_DATA/Transmission/transmission.sh
-echo "Transmission script done." | ts "%F %H:%M:%.S" >> $log
+echo "$(date '+%F %T') Transmission script done." >> $log
 
-echo "Copying /usr/local/etc/services to /etc/services" | ts "%F %H:%M:%.S" >> $log
+echo "$(date '+%F %T') Copying /usr/local/etc/services to /etc/services" >> $log
 cp -f /usr/local/etc/services /etc/services
 sleep 2
 export OPTWARE_TARGET=cs08q1armel
-echo "Starting xinetd..." | ts "%F %H:%M:%.S" >> $log
+echo "$(date '+%F %T') Starting xinetd..." >> $log
 if [ -e "/opt/sbin/xinetd" ]
 	then
 		/sbin/daemon_mgr xinetd start "/opt/sbin/xinetd"
-		echo "xinetd started" | ts "%F %H:%M:%.S" >> $log
+		echo "$(date '+%F %T') xinetd started" >> $log
 #		sleep 5
 	else
-		echo "xinetd not accessible" | ts "%F %H:%M:%.S" >> $log
+		echo "$(date '+%F %T') xinetd not accessible" >> $log
 fi
 #echo "Closing output redirection to log file"
 ## close the stderr and stdout file descriptors.
@@ -93,4 +93,4 @@ fi
 #wait $ts_pid
 ##delete named pipe when finished
 #trap 'rm "$namedpipe"' EXIT
-echo "*** End of autorunmaster.sh" | ts "%F %H:%M:%.S" >> $log
+echo "$(date '+%F %T') *** End of autorunmaster.sh" >> $log
