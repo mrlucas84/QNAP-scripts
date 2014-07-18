88e061a15437ff554c72ff24dbabd72563c5afc8
danir0d <rodriguez.doallo@gmail.com>
2014-07-18 07:56:24 +0200

    clean out commented code and background call transmission

--- autorunmaster/autorunmaster.sh
+++ autorunmaster/autorunmaster.sh
@@ -3,31 +3,17 @@
 # con /bin/sh no funciona el redireccionamiento a funcion/process substitution
 # this is called by autorun.sh
 # /share/HDA_DATA/.qpkg/autorun/autorunmaster.sh
-#alias ts='/opt/bin/ts'
+
 log=/share/HDA_DATA/.qpkg/autorun/autorunmaster.log
-#stdlog=/share/HDA_DATA/.qpkg/autorun/autorunmaster_std.log
 log(){
 	/bin/echo "$(/bin/date '+%F %T.%3N') $1" >> $log
 }
 log "***** Starting autorunmaster.sh *****"
-#namedpipe=/share/HDA_DATA/.qpkg/autorun/autorunmaster.sh.pipe
 apache_conf=/etc/config/apache/apache.conf
 apache_custom_conf=/share/HDA_DATA/apache/apache-custom.conf
-#if [ -p $namedpipe ]; then
-#	rm -f "$namedpipe"
-#fi
-## create named pipe
-#mkfifo $namedpipe
-## Start ts writing to a logfile, but pulling its input from our named pipe.
-#ts "%F %H:%M:%.S" >> $log < $namedpipe &
-## capture ts's process ID for the wait command.
-#ts_pid=$!
-## redirect the rest of the stderr and stdout to our named pipe.
-#exec > $namedpipe 2>&1
-exec >> $log 2>&1
-#echo "*** Starting autorunmaster.sh"
 
-#echo "PID of ts: $ts_pid"
+exec >> $log 2>&1
+
 # adding IPKG apps into system path ... 
 # Dani 12/11/2011 ESTO SE HACE EN /opt/Optware.sh 
 #/bin/cat /etc/profile | /bin/grep "PATH" | /bin/grep "/opt/bin" 1>>/dev/null 2>>/dev/null
@@ -61,11 +47,6 @@ if [ $? -eq 0 ]
 		log "Restarting apache"
 		/etc/init.d/Qthttpd.sh restart
 fi
-#echo "Copying apache SSL custom conf" >> $log
-#cp -f /share/HDA_DATA/apache/extra/apache-ssl.conf /etc/config/apache/extra/apache-ssl.conf
-
-#echo "Restarting apache" >> $log
-#/etc/init.d/Qthttpd.sh restart >> $log
 
 # crea enlace a script de transmission para que se ejecute cuando toca.
 #echo "Creating symlink /etc/rcS.d/QS901transmission -> /share/HDA_DATA/Transmission/transmission.sh"
@@ -73,8 +54,8 @@ fi
 
 #Dani 12/11/2011
 log "Calling Transmission script /share/HDA_DATA/Transmission/transmission.sh"
-/share/HDA_DATA/Transmission/transmission.sh
-log "Transmission script done."
+/share/HDA_DATA/Transmission/transmission.sh &
+#log "Transmission script done."
 
 log "Copying /usr/local/etc/services to /etc/services"
 /bin/cp -f /usr/local/etc/services /etc/services
@@ -89,14 +70,6 @@ if [ -e "/opt/sbin/xinetd" ]
 	else
 		log "xinetd not accessible"
 fi
-#echo "Closing output redirection to log file"
-## close the stderr and stdout file descriptors.
-#exec 1>&- 2>&-
-#
-## Wait for ts to finish since now that other end of the pipe has closed.
-#wait $ts_pid
-##delete named pipe when finished
-#trap 'rm "$namedpipe"' EXIT
 log "***** End of autorunmaster.sh *****"
 /bin/sync
 /bin/sleep 1
