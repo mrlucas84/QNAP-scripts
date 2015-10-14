379d73d209b8e36d36f9f79b6a827702a02d0518
b4nd0ler0 <b4nd0ler0@hotmail.com>
2015-10-14 12:02:05 +0200

    Removed PATH config, Entware start, Transmission start

--- autorunmaster/autorunmaster.sh
+++ autorunmaster/autorunmaster.sh
@@ -15,24 +15,9 @@ apache_custom_conf=/share/CACHEDEV1_DATA/myprograms/apache/apache-custom.conf
 
 exec >> $log 2>&1
 
-# adding IPKG apps into system path ... 
-# Dani 12/11/2011 ESTO SE HACE EN /opt/Entware.sh 
-#/bin/cat /etc/profile | /bin/grep "PATH" | /bin/grep "/opt/bin" 1>>/dev/null 2>>/dev/null
-# Bug fix for following: put IPKG first, per http://forum.qnap.com/viewtopic.php?f=124&t=15663
-# was [ $? -ne 0 ] && /bin/echo "export PATH=$PATH":/opt/bin:/opt/sbin >> /etc/profile
-#[ $? -ne 0 ] && /bin/echo "export PATH=/opt/bin:/opt/sbin:\$PATH" >> /etc/profile
 log "PATH=$PATH"
-
-#Dani 07/10/2015 esto se inicia automáticamente vía /etc/init.d/Entware.sh->/share/CACHEDEV1_DATA/.qpkg/Entware/Entware.sh
-#Dani 01/10/2015 modificado segun http://forum.qnap.com/viewtopic.php?f=85&t=18977
-#FIRST start Entware and delete the /etc/rcS.d/QS10.... script
-#log "Starting Entware"
-#/etc/init.d/Entware.sh start
-#/bin/rm -f /etc/rcS.d/QS105Entware
-#log "Entware started"
-# Fin Dani 01/10/2015
-
 log "Setting up custom scripts"
+
 #sobreescribir config SSH con la propia 
 # log "Delete /etc/ssh/sshd_config and recreate as symlink to /share/HDA_DATA/ssh/sshd_config"
 # /bin/rm -f /etc/ssh/sshd_config
@@ -48,17 +33,6 @@ else
 	log "Restarting apache"
 	/etc/init.d/Qthttpd.sh restart
 fi
-
-#Dani 07/10/2015 esto se inicia automáticamente desde /etc/init.d/Entware.sh que 
-#a la larga llama /share/CACHEDEV1_DATA/.qpkg/Entware/etc/init.d/S88transmission
-# crea enlace a script de transmission para que se ejecute cuando toca.
-#echo "Creating symlink /etc/rcS.d/QS901transmission -> /share/HDA_DATA/Transmission/transmission.sh"
-#/bin/ln -sf /share/HDA_DATA/Transmission/transmission.sh /etc/rcS.d/QS901transmission
-
-#Dani 12/11/2011
-# log "Calling Transmission script /share/HDA_DATA/Transmission/transmission.sh"
-# /share/HDA_DATA/Transmission/transmission.sh &
-#log "Transmission script done."
 
 # log "Copying /usr/local/etc/services to /etc/services"
 # /bin/cp -f /usr/local/etc/services /etc/services
