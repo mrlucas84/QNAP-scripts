cd84ab4706b739a63155da7d7dd7a6288b24800d
danirod <8984@bancamarch.es>
2015-10-14 17:48:07 +0200

    Test script simplified

--- autorunmaster/autoruntest2.sh
+++ autorunmaster/autoruntest2.sh
@@ -1,19 +1,14 @@
-#!/bin/sh                                                                                                                                                        
-# con /opt/bin/bash parece que no se lanza en boot                                                                                                               
-# con /bin/sh no funciona el redireccionamiento a funcion/process substitution                                                                                   
-log=autoruntest.log                                                                                                                                              
-stdlog=autoruntest_std.log                                                                                                                                       
-log(){                                                                                                                                                           
-        echo "[1] $(/bin/date '+%F %T.%3N') $1" >> $log                                                                                                          
-        echo "" >> $log                                                                                                                                          
-#       printf "[4] %.23s" $(date +'%Y-%m-%dT%H:%M:%S.%N') >> $log                                                                                               
-}                                                                                                                                                                
-log "*** Starting autoruntest.sh"                                                                                                                                
-exec > $stdlog 2>&1                                                                                                                                              
-                                                                                                                                                                 
-log "PATH=$PATH"                                                                                                                                                 
-log "Running command with stdout output"                                                                                                                         
-ls -la                                                                                                                                                           
-log "Running wrong command with stderr output"                                                                                                                   
-gfrxls                                                                                                                                                           
-log "*** End of autoruntest.sh"  
+#!/bin/sh
+# con /opt/bin/bash parece que no se lanza en boot
+# con /bin/sh no funciona el redireccionamiento a funcion/process substitution
+log=./autoruntest.log
+stdlog=./autoruntest_std.log
+/bin/echo "$(/bin/date '+%F %T.%3N') *** Starting autoruntest.sh" >> $log
+exec > $stdlog 2>&1
+
+/bin/echo "$(/bin/date '+%F %T.%3N') PATH=$PATH" >> $log
+/bin/echo "$(/bin/date '+%F %T.%3N') Running command with stdout output" >> $log
+/bin/ls -la
+/bin/echo "$(/bin/date '+%F %T.%3N') Running wrong command with stderr output" >> $log
+gfrxls
+/bin/echo "$(/bin/date '+%F %T.%3N') *** End of autoruntest.sh" >> $log
