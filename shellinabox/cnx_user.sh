5077092fe56597b735fef836d6f8bd510fd09c41
b4nd0ler0 <b4nd0ler0@hotmail.com>
2015-10-20 11:41:31 +0200

    Añadido cnx_user.sh original del qpkg descargado http://forum.qnap.com/viewtopic.php?f=132&t=78981#p480460

---
+++ shellinabox/cnx_user.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+trap '{ echo "Hey, you pressed Ctrl-C.  Time to quit." ; exit 1; }' INT
+echo -n "Enter your name (in next 10 seconds) and press [ENTER]: "
+read -t 10 var_name
+if [ -z $var_name ] ; then
+	exit 1
+fi
+ADM_ONLY=`/sbin/getcfg SIAB Auth_user -d "ADMIN" -u -f /myprog/shellinabox/shellinabox.conf`
+case "$ADM_ONLY" in
+	ADMIN)
+        if [ "$var_name" != "admin" ] ; then
+                echo "Hum! only administrator user is authorize ... exit"
+                exit 1
+        fi
+	;;
+	ALL)
+        /bin/grep -q $var_name /etc/passwd
+        if [ $? -ne 0 ] ; then
+                echo "Hum! unknown name .... exit"
+                exit 1
+        fi
+	;;
+	LIST)
+        /bin/grep -q $var_name /myprog/shellinabox/user.lst
+        if [ $? -ne 0 ] ; then
+                echo "Hum! unknown name .... exit"
+                exit 1
+        fi
+	;;
+	*)
+	if [ "$var_name" != "admin" ] ; then
+                echo "Hum! Default ... only administrator user is authorize ... exit"
+                exit 1
+        fi
+        ;;
+esac
+/myprog/shellinabox/bin/cnx_user -  $var_name 
+if [ $? -eq 0 ] ; then
+	echo "Bye bye"
+else
+	echo "Bad password or empty"
+fi
+exit
