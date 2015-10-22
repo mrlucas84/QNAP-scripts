4f89574765ac64f85db4b9a1d66462befeb1ce49
b4nd0ler0 <b4nd0ler0@hotmail.com>
2015-10-22 12:17:09 +0200

    Mod. paths

--- logrotate/logrotate.sh
+++ logrotate/logrotate.sh
@@ -1,9 +1,10 @@
 #!/opt/bin/bash
 # script called by cron
+# /share/CACHEDEV1_DATA/myprograms/logrotate/logrotate.sh
 timestamp_log() { while IFS='' read -r line; do echo "[$(date '+%F %T')] $line" >> "$1"; done; };
-LOG=/share/HDA_DATA/logrotate/logrotate.log
+LOG=/share/CACHEDEV1_DATA/myprograms/logrotate/logrotate.log
 #exec 2>&1>>"$LOG"
 exec 2>&1> >(timestamp_log $LOG)
 echo "Starting logrotate.sh"
-/opt/sbin/logrotate /share/HDA_DATA/logrotate/logrotate.conf
+/opt/sbin/logrotate /share/CACHEDEV1_DATA/myprograms/logrotate/logrotate.conf
 echo "End of logrotate.sh"
